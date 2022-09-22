8944676f8be653abcceb04d4d785ddcb4afeeb59
Manokamna Singh <85088233+Manokamnasingh1@users.noreply.github.com>
2022-09-22 19:00:30 +0530

    reverse string

--- src/reversestr.java
+++ src/reversestr.java
@@ -1,2 +1,15 @@
-package PACKAGE_NAME;public class reversestr {
+public class reversestr {
+    public static void printrev( String str, int indx){
+        if(indx==0){
+            System.out.println(str.charAt(indx));
+            return;
+        }
+        System.out.print(str.charAt(indx));
+        printrev(str,indx-1);
+    }
+    public static void main(String args[]){
+        String str = "abcd";
+        printrev(str,str.length()-1);
+
+    }
 }
